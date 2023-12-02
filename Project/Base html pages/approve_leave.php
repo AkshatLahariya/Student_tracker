bbbc7252f3cf964ce009ce22bd53115dcb60356b
Akshat Lahariya <1032211891@mitwpu.edu.in>
2023-12-03 01:23:57 +0530

    Made some changes to admin dashboard ,approve leave and made approve php

---
+++ Project/Base html pages/approve_leave.php
@@ -0,0 +1,30 @@
+<?php
+// Replace these database connection details with your actual credentials
+$servername = "localhost";
+$username = "Akshat";
+$password = "12345";
+$dbname = "user";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve pending leave approval requests
+$sql = "SELECT * FROM leave_requests WHERE status = 'pending'";
+$result = $conn->query($sql);
+
+// Display pending leave approval requests
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<p><strong>Faculty Name:</strong> " . $row["faculty_name"] . "<br><strong>Faculty ID:</strong> " . $row["faculty_id"] . "</p>";
+    }
+} else {
+    echo "No pending leave approval requests.";
+}
+
+$conn->close();
+?>
