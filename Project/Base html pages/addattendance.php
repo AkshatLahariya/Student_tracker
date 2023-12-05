fecad9faa580f33ffd993eeb8e1d322517e30d87
Dchauhan629 <146003427+Dchauhan629@users.noreply.github.com>
2023-12-05 21:15:43 +0530

    Updated addattendance.php

--- Project/Base html pages/addattendance.php
+++ Project/Base html pages/addattendance.php
@@ -2,16 +2,18 @@
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Extract form data
-    $panel = $_POST["name"];
-    $rollNumber = $_POST["roll_number"];
-    $date = $_POST["date"];
-    $attendanceStatus = $_POST["attendance"];
+    $name = isset($_POST["name"]) ? $_POST["name"] : '';
+    $rollNumber = isset($_POST["roll_number"]) ? $_POST["roll_number"] : '';
+    $PRN = isset($_POST["PRN"]) ? $_POST["PRN"] : '';
+    $date = isset($_POST["date"]) ? $_POST["date"] : '';
+    $attendance = isset($_POST["attendance"]) ? $_POST["attendance"] : '';
 
-    // Validate and sanitize the data (you may need more robust validation)
+    // Validate and sanitize the data
     $name = htmlspecialchars($name);
-    $rollNumber = intval($rollNumber); // Convert to integer
+    $rollNumber = intval($rollNumber);
+    $PRN = intval($PRN);// Convert to integer
     $date = htmlspecialchars($date);
-    $attendanceStatus = ($attendanceStatus == 'present') ? 'Present' : 'Absent'; // Sanitize attendance status
+    $attendance = ($attendance == 'present') ? 'Present' : 'Absent'; // Sanitize attendance status
 
     // Perform database operations (replace this with your actual database logic)
     // For demonstration, let's assume you have a MySQL database
@@ -20,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = "";
     $dbname = "student_tracker";
 
-    
+    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
@@ -28,13 +30,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Prepare and execute SQL query
-    $sql = "INSERT INTO attendance_table (panel, roll_number, date, status) VALUES ('$panel', $rollNumber, '$date', '$attendanceStatus')";
+    // Prepare and execute SQL query using prepared statement
+    $sql = "INSERT INTO Student_table (Name, Roll_number, PRN, Date, Attendance) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Attendance added successfully";
+    if ($stmt) {
+        // Bind parameters and execute
+        $stmt->bind_param("siss", $name, $rollNumber, $PRN $date, $attendance);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            echo "Attendance added successfully";
+        } else {
+            echo "Error: Unable to add attendance";
+        }
+
+        // Close the statement
+        $stmt->close();
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $conn->error;
     }
 
     // Close the database connection
@@ -51,51 +65,116 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Attendance - College ERP</title>
-    <link rel="stylesheet" href="addattendance.css">
     <style>
-        /* Add some spacing between input fields */
-        label,
-        input,
-        select,
-        button {
-            display: block;
-            margin-bottom: 10px;
-        }
+/* Reset CSS */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-        /* Display radio buttons and labels on the same line */
-        .attendance-group label {
-            display: inline-flex;
-            align-items: center;
-        }
+/* Container styling */
+.dashboard-container {
+    width: 100%;
+    height: 100vh;
+    background-image: url('attendance.png'); /* Use the correct path */
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 10px; 
+}
 
-        /* Add line breaks after each radio button group */
-        .attendance-group {
-            margin-bottom: 10px;
-        }
+/* Form styling */
+.dashboard-container h2 {
+    font-size: 28px;
+    margin-bottom: 20px;
+    color: #0d0d0d; /* Set text color */
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold; /* Make the text bold */
+    text-transform: uppercase; /* Convert text to uppercase */
+    text-align: center; /* Align the text to the right */
+    text-decoration: underline; 
+}
 
-        /* Display radio buttons and labels on separate lines */
-        input[type="radio"],
-        label {
-            display: inline-block;
-            margin-right: 10px; /* Adjust the margin as needed */
-        }
+.dashboard-container form {
+    width: 80%;
+    max-width: 400px;
+    background: #fff;
+    padding: 20px;
+    margin-right: 200px;
+    margin-bottom: 0px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-weight: bold;
+    border-radius: 8px;
+    border: 4px solid #000000;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 
-        /* Add line breaks after each radio button group */
-        .attendance-group {
-            margin-bottom: 10px;
-        }
+/* Input field styling */
+label,
+select,
+input,
+button {
+    display: block;
+    margin-bottom: 15px;
+    width: 100%;
+}
+
+/* Adjust radio button and label display */
+.attendance-group label {
+    display: inline-flex;
+    align-items: center;
+    margin-bottom: 5px;
+    color: #000000;
+    white-space: nowrap; /* Ensure that the text does not wrap to the next line */
+}
+
+.attendance-group input[type="radio"] {
+    margin-right: 5px; /* Add spacing between the radio button and label */
+}
+
+
+
+/* Adjust color when radio button is checked */
+#present:checked + label {
+    color: #4caf50;
+}
+
+#absent:checked + label {
+    color: #ff4c4c;
+}
+
+/* Button styling */
+button {
+    background-color: #3c00a0;
+    color: #fff;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+button:hover {
+    background-color: #2a0070;
+}
     </style>
 </head>
 <body>
-
     <div class="dashboard-container">
-        <form action="addattendance.php" method="post">
+        
+        <form action="" method="post">
             <h2>Add Attendance</h2>
+           
 
-            
-
-            <label for="name">Roll Number:</label>
-            <input type="text" id="name" name="name" required>
+            <label for="Name">Name:</label>
+            <input type="text" id="Name" name="Name" required>
+			
+			<label for="PRN">PRN:</label>
+            <input type="number" id="PRN" name="PRN" required>
 
             <label for="roll_number">Roll Number:</label>
             <input type="number" id="roll_number" name="roll_number" required>
@@ -111,6 +190,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <input type="radio" id="absent" name="attendance" value="absent" required>
             <label for="absent">Absent</label>
+
+            
 
             <br> <!-- Add a line break for a one-line gap -->
 
