93ff26bc3fbeecbc169b6acf961f0299873d1431
Dchauhan629 <146003427+Dchauhan629@users.noreply.github.com>
2023-12-05 22:21:42 +0530

    Updated viewattendance.php

--- Project/Base html pages/viewattendance.php
+++ Project/Base html pages/viewattendance.php
@@ -1,155 +1,74 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "student_tracker";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Replace this with your actual database query logic
-$subjectsData = array(
-    array('name' => 'AIES', 'progressBarId' => 'aiesProgressBar'),
-    array('name' => 'FSD', 'progressBarId' => 'fsdProgressBar'),
-    array('name' => 'DEC', 'progressBarId' => 'decProgressBar'),
-    array('name' => 'ICS', 'progressBarId' => 'icsProgressBar'),
-    array('name' => 'ITCH', 'progressBarId' => 'itchProgressBar')
-);
-
-$data = array(
-    'totalPresent' => 75,
-    'totalAbsent' => 25,
-    'overallPercentage' => 75,
-    'subjects' => array()
-);
-
-// Loop through each subject and fetch data
-foreach ($subjectsData as $subjectData) {
-    $subjectName = $subjectData['name'];
-    $progressBarId = $subjectData['progressBarId'];
-
-    // Replace this with your actual database query for each subject
-    $present = 15;  // Replace with actual present count
-    $absent = 5;    // Replace with actual absent count
-    $total = $present + $absent;
-    $percentage = ($present / $total) * 100;
-
-    // Add subject data to the $data array
-    $data['subjects'][] = array(
-        'name' => $subjectName,
-        'progressBarId' => $progressBarId,
-        'present' => $present,
-        'absent' => $absent,
-        'percentage' => $percentage
-    );
-}
-
-// Set response headers to indicate JSON content
-header('Content-Type: application/json');
-
-// Output the data as JSON
-echo json_encode($data);
-
-// Close the database connection
-$conn->close();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attendance Analysis - College ERP</title>
-    <link rel="stylesheet" href="viewattendance.css">
+    <title>Student Attendance</title>
+<style>
+        /* CSS for basic table formatting */
+        body {
+            font-family: Arial, sans-serif; /* Change the font if needed */
+            background-color: #f4f4f4; /* Set a background color */
+            margin: 0;
+            padding: 0;
+        }
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin-top: 20px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 10px; /* Increased padding for better spacing */
+        }
+        th {
+            background-color: #3c00a0;
+            color: #fff;
+        }
+    </style>
 </head>
 <body>
-    <div class="dashboard-container">
-        <h2>Attendance Analysis</h2>
-        
-        <h3>Overall Attendance</h3>
-        <p>Total Present: <span id="totalPresent">0</span></p>
-        <p>Total Absent: <span id="totalAbsent">0</span></p>
-        
-        <!-- Update the Overall Attendance Percentage section -->
-        <p>Overall Attendance Percentage:
-            <div class="circular-chart">
-                <div class="circle" id="overallProgressBar"></div>
-                <div class="percent" id="overallPercentage">0%</div>
-            </div>
-        </p>
+    <h2>Student Attendance</h2>
 
-        <h3>Subject-wise Attendance</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th>Subject</th>
-                    <th>Total Present</th>
-                    <th>Total Absent</th>
-                    <th>Attendance Percentage</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>FSD</td>
-                    <td id="fsdPresent">0</td>
-                    <td id="fsdAbsent">0</td>
-                    <td>
-                        <span id="fsdPercentage">0%</span>
-                        <div class="progress-bar-container">
-                            <div id="fsdProgressBar" class="progress-bar"></div>
-                        </div>
-                    </td>
-                </tr>
-                <!-- Add rows for other subjects as needed -->
-            </tbody>
-        </table>
-    </div>
-    
-   <!-- Update the script section -->
-   <script>
-       function fetchAttendanceData() {
-           fetch('addattendance.php')
-               .then(response => response.json())
-               .then(data => {
-                   updateAttendanceUI(data);
-               })
-               .catch(error => console.error('Error fetching attendance data:', error));
-       }
+    <?php
+    // Establish database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "user"; // Replace with your actual database name
 
-       function updateAttendanceUI(data) {
-           document.getElementById('totalPresent').innerText = data.totalPresent;
-           document.getElementById('totalAbsent').innerText = data.totalAbsent;
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-           setOverallAttendanceValues('overallPercentage', 'overallProgressBar', data.overallPercentage);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-           data.subjects.forEach(subject => {
-               setSubjectAttendanceValues(subject.name, subject.progressBarId, subject.present, subject.absent, subject.percentage);
-           });
-       }
+    // Retrieve form data
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $Name = $_POST['Name'];
+        $Roll_number = $_POST['Roll_number'];
 
-       function setOverallAttendanceValues(percentageId, progressBarId, percentage) {
-           document.getElementById(percentageId).innerText = `${percentage}%`;
-           const progressBar = document.getElementById(progressBarId);
-           progressBar.style.width = `${percentage}%`;
-       }
+        // Query to fetch attendance data for the student
+        $sql = "SELECT Date, Attendance FROM your_table_name WHERE Name = '$Name' AND Roll_number = '$Roll_number' AND PRN='$PRN'";
+        $result = $conn->query($sql);
 
-       function setSubjectAttendanceValues(subject, progressBarId, present, absent, percentage) {
-           document.getElementById(`${subject}Present`).innerText = present;
-           document.getElementById(`${subject}Absent`).innerText = absent;
-           document.getElementById(`${subject}Percentage`).innerText = `${percentage.toFixed(2)}%`;
+        if ($result->num_rows > 0) {
+            echo "<h3>Attendance for $Name (Roll Number: $Roll_number) PRN: $PRN</h3>";
+            echo "<table>";
+            echo "<tr><th>Date</th><th>Attendance</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row['Date'] . "</td><td>" . $row['Attendance'] . "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No attendance records found for $Name (Roll Number: $Roll_number) PRN: $PRN";
+        }
+    }
 
-           const progressBar = document.getElementById(progressBarId);
-           progressBar.style.width = `${percentage}%`;
-       }
-
-       // Call the function to fetch data
-       fetchAttendanceData();
-   </script>
+    // Close the database connection
+    $conn->close();
+    ?>
 </body>
 </html>
-
-
