6107a4e2f1a72618ca4de7ad3453a9bb244a89bf
Dchauhan629 <146003427+Dchauhan629@users.noreply.github.com>
2023-12-06 09:02:46 +0530

    Update viewattendance.php

--- Project/Base html pages/viewattendance.php
+++ Project/Base html pages/viewattendance.php
@@ -51,7 +51,7 @@
         $Roll_number = $_POST['Roll_number'];
 
         // Query to fetch attendance data for the student
-        $sql = "SELECT Date, Attendance FROM student_table WHERE Name = '$Name' AND Roll_number = '$Roll_number' AND PRN='$PRN'";
+        $sql = "SELECT Date, Attendance FROM Student_table WHERE Name = '$Name' AND Roll_number = '$Roll_number' AND PRN='$PRN'";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
